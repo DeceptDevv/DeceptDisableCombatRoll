8021d6611e18bd4d2d55f50e12ab2035f52adb10
DeceptDevv <130248921+DeceptDevv@users.noreply.github.com>
2023-04-08 13:08:03 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,8 @@
 fx_version 'cerulean'
 
 game 'gta5'
-author 'ZAD4Y'
-description 'https://github.com/DeceptDevv/Deceptdevv-npc'
+author 'Decept'
+description 'https://github.com/DeceptDevv/DeceptDisableCombatRoll'
 version '1.0.1'
 
 client_script 'main.lua'
